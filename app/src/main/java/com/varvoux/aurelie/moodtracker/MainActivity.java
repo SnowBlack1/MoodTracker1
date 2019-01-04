10c269f0f179d445775c8a7c56451f17ed112daa
SnowBlack1 <varvoux.aurelie@gmail.com>
2019-01-04 15:03:31 +0100

    Initial commit

---
+++ app/src/main/java/com/varvoux/aurelie/moodtracker/MainActivity.java
@@ -0,0 +1,15 @@
+package com.varvoux.aurelie.moodtracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
